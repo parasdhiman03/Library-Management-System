0355d4b507261502af43dcf1ccf51d4e63182596
Paras <dhimanparas000@gmail.com>
2023-02-18 10:24:09 +0530

    Initial commit

---
+++ index.php
@@ -0,0 +1,59 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>
+		Online Library Management System
+	</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<style type="text/css">
+	nav
+	{
+		float: right;
+		word-spacing: 30px;
+		padding: 20px;
+	}
+	nav li 
+	{
+		display: inline-block;
+		line-height: 80px;
+	}
+</style>
+</head>
+
+
+<body>
+	<div class="wrapper">
+		<header>
+		<div class="logo">
+			<img src="images/2.jpg">
+			<h1 style="color: white;">ONLINE LIBRARY MANAGEMENT SYSTEM</h1>
+		</div>
+			<nav>
+				<ul>
+					<li><a href="index.php">HOME</a></li>
+					<li><a href="books.php">BOOKS</a></li>
+					<li><a href="login.php">LOGIN</a></li>
+					<li><a href="feedback.php">FEEDBACK</a></li>
+				</ul>
+			</nav>
+		</header>
+		<section>
+		<div class="sec_img">
+			<br><br><br>
+		</div>
+		</section>
+		<footer>
+			<p style="color:white;  text-align: center; ">
+				<br><br>
+				Email: dhimanparas000@gmail.com <br>
+				Mobile: +918532886370
+			</p>
+		</footer>
+	</div>
+</body>
+
+</html>
